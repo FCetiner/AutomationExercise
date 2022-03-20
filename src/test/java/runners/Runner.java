eca93154a76338167d5115c4bb860258b39b6ec0
Ferhat <fcetinerr@gmail.com>
2022-03-20 10:58:51 +0300

    C06

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/Pcucumber.xml"},
         features="src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@US005",
+        tags = "@US006",
         dryRun =false
 )
 public class Runner {
